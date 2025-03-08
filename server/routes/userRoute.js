f716d2f43bd6c8e0e80302332043969d7b7ec181
poojasingh5555 <singhpoojareckofficial@gmail.com>
2025-03-08 23:13:06 +0530

    added screenshots and modified routes

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { userLogin, userSignup } from "../controllers/userController.js";
+import { fetchChats, fetchUsers, userLogin, userSignup } from "../controllers/userController.js";
 const router = express.Router();
 
 router.route("/register").post(userSignup);
 router.route("/login").post(userLogin);
-router.route("/fetch-users").get(userLogin);
-router.route("/fetch-chats/:id").get(userLogin);
+router.route("/fetch-users").get(fetchUsers);
+router.route("/fetch-chats/:id").get(fetchChats);
 
 
 
