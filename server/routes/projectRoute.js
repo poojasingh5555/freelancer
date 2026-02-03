5eb79392073d24ff39b32dac58b17e5030fbb5f2
poojasingh5555 <singhpoojareckofficial@gmail.com>
2026-02-03 19:47:20 +0530

    changes

--- server/routes/projectRoute.js
+++ server/routes/projectRoute.js
@@ -10,8 +10,6 @@ const router = express.Router();
 router.route("/fetch-project/:id").get(getProject);
 router.route("/fetch-projects").get(getAllProjects);
 router.route("/new-project").post(addNewProject);
-router.route("/submit-project").post(submitProject);
-
-
+router.route("/project").post(submitProject);
 
 export default router;
